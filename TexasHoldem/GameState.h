2bb5ff66ab9f66d0174807eaf09994b9686dc239
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-04-06 16:13:57 -0400

    efv

--- TexasHoldem/GameState.h
+++ TexasHoldem/GameState.h
@@ -1,1 +1,28 @@
-#pragma once
+/**
+ *\file card.h
+ * \brief Prototype od the card class
+ * \author Samuel Ouellet (111 233 977)
+ */
+
+#ifndef GAMESTATE_H
+#define GAMESTATE_H
+
+#include <string>
+#include "contract_exception.h"
+
+
+namespace game {
+
+    class GameState {
+
+    private:
+        int m_iteration;
+        int m_hmPlayer;
+
+    public:
+        TexasHoldemGame(int p_hmPlayer);
+
+    };
+}
+
+#endif //GAMESTATE_H
--- TexasHoldem/TexasHoldemGame.h
+++ TexasHoldem/TexasHoldemGame.h
@@ -7,8 +7,13 @@
 #ifndef TEXASHOLDEMGAME_H
 #define TEXASHOLDEMGAME_H
 
+#include <iostream> 
+#include <vector>
 #include <string>
+#include <bits/stdc++.h> 
 #include "contract_exception.h"
+#include "Card.h"
+#include "player.h"
 
 
 namespace game {
@@ -17,14 +22,21 @@ namespace game {
 
         struct GameState {
             int iteration;
+            int hmPlayer;
+            std::vector<Card> cardsOnTable;
+            std::vector<Player> players;
         };
 
     private:
         int m_iteration;
         int m_hmPlayer;
+        GameState m_currentGameState;
 
     public:
         TexasHoldemGame(int p_hmPlayer);
+
+        static GameState getRandomNextState(GameState p_gameState);
+        GameState getState();
 
     };
 }
